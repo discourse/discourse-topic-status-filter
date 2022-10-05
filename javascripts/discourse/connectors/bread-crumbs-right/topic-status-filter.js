06fbb729e19af518e5fa455465dd9f0434fd3e2c
David Taylor <david@taylorhq.com>
2022-10-05 23:54:04 +0100

    Modernise and fix behavior when used with loading-slider

--- javascripts/discourse/connectors/bread-crumbs-right/topic-status-filter.js
+++ javascripts/discourse/connectors/bread-crumbs-right/topic-status-filter.js
@@ -1,60 +1,26 @@
-import DiscourseUrl from "discourse/lib/url";
+const enabledCategoryIds = settings.enabled_categories
+  .split("|")
+  .filter(Boolean)
+  .map((id) => parseInt(id, 10));
 
 export default {
-  shouldRender(args, component) {
-    const ids = settings.enabled_categories.split("|").filter((n) => n);
-
-    if (ids.length == 0) {
-      return true;
-    }
-
-    const category = component.get("parentView.category");
-    if (category) {
-      return ids.includes(category.id.toString());
-    }
-
-    return false;
-  },
-
   setupComponent(args, component) {
-    const statuses = ["all", "open", "closed"].map((status) => {
-      return {
-        name: I18n.t(themePrefix(`topic_status_filter.${status}`)),
-        value: status,
-      };
-    });
-    component.set("statuses", statuses);
-
-    const queryStrings = window.location.search;
-    if (queryStrings.match(/status=open/)) {
-      component.set("status", "open");
-    } else if (queryStrings.match(/status=closed/)) {
-      component.set("status", "closed");
-    } else {
-      component.set("status", "all");
-    }
-  },
-
-  actions: {
-    changeStatus(newStatus) {
-      let location = window.location;
-      let queryStrings = location.search;
-      let params = queryStrings.startsWith("?")
-        ? queryStrings.substr(1).split("&")
-        : [];
-
-      params = params.filter((param) => {
-        return !param.startsWith("status=");
-      });
-
-      if (newStatus && newStatus !== "all") {
-        params.push(`status=${newStatus}`);
+    // Unfortunately `shouldRender` is not re-evaluated when the arguments change.
+    // This is a workaround to set `display: none` when the connector is not required.
+    component.classNameBindings = ["hidden"];
+    component.didReceiveAttrs = function () {
+      if (enabledCategoryIds.length === 0) {
+        this.set("hidden", false);
+      } else if (
+        this.currentCategory &&
+        enabledCategoryIds.includes(this.currentCategory.id)
+      ) {
+        this.set("hidden", false);
+      } else {
+        this.set("hidden", true);
       }
 
-      queryStrings = params.length > 0 ? `?${params.join("&")}` : "";
-      DiscourseUrl.routeTo(
-        `${location.pathname}${queryStrings}${location.hash}`
-      );
-    },
+      this._super();
+    };
   },
 };
